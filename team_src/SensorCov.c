e088bc807a23ff69730ba4bdca3bc132a2723d17
Nathan Lord <lord.59@osu.edu>
2015-03-12 19:00:55 -0400

    Working with Servos

--- team_src/SensorCov.c
+++ team_src/SensorCov.c
@@ -52,19 +52,19 @@ void SensorCovInit()
 	//todo USER: SensorCovInit()
 
 	//set up mpu int pin
-	EALLOW;
-	GpioCtrlRegs.GPAMUX2.bit.GPIO23 = 0;         // GPIO
-	GpioCtrlRegs.GPADIR.bit.GPIO23 = 0;          // input
-	GpioCtrlRegs.GPAQSEL2.bit.GPIO23 = 0;        //Synch to SYSCLKOUT only
-	GpioCtrlRegs.GPAPUD.bit.GPIO23 = 1; 		//disable pull up
-	EDIS;
-
-	EALLOW;
-	GpioCtrlRegs.GPAMUX1.bit.GPIO15 = 0;         // GPIO
-	GpioCtrlRegs.GPADIR.bit.GPIO15 = 1;          // input
-	GpioCtrlRegs.GPAQSEL1.bit.GPIO15 = 0;        //Synch to SYSCLKOUT only
-	GpioCtrlRegs.GPAPUD.bit.GPIO15 = 1; 		//disable pull up
-	EDIS;
+//	EALLOW;
+//	GpioCtrlRegs.GPAMUX2.bit.GPIO23 = 0;         // GPIO
+//	GpioCtrlRegs.GPADIR.bit.GPIO23 = 0;          // input
+//	GpioCtrlRegs.GPAQSEL2.bit.GPIO23 = 0;        //Synch to SYSCLKOUT only
+//	GpioCtrlRegs.GPAPUD.bit.GPIO23 = 1; 		//disable pull up
+//	EDIS;
+//
+//	EALLOW;
+//	GpioCtrlRegs.GPAMUX1.bit.GPIO15 = 0;         // GPIO
+//	GpioCtrlRegs.GPADIR.bit.GPIO15 = 1;          // input
+//	GpioCtrlRegs.GPAQSEL1.bit.GPIO15 = 0;        //Synch to SYSCLKOUT only
+//	GpioCtrlRegs.GPAPUD.bit.GPIO15 = 1; 		//disable pull up
+//	EDIS;
 
 //	spi_fifo_init();
 //	SpiGpio();
@@ -73,43 +73,42 @@ void SensorCovInit()
 	s1 = 0.5;
 	s2 = 0.5;
 
-//	I2CA_Init();
-//
-//	devAddr = MPU6050_DEFAULT_ADDRESS;
-//
-//	mpu_reset();
-//	int id = getDeviceID();
-//	setSleepEnabled(false);
-//	char status = dmpInitialize();
-//	//setClockSource(MPU6050_CLOCK_PLL_XGYRO);
-//
-//    setFullScaleGyroRange(MPU6050_GYRO_FS_250);
-//    setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
-//
-//
-////    setXGyroOffset(-3500);
-////    setYGyroOffset(-365);
-////    setZGyroOffset(210);
-////    setZAccelOffset(914);
-////    setXAccelOffset(-980);
-////    setYAccelOffset(630);
-//
-//
-//
-//	adcinit();
-//
-//	//CONFIG GP_BUTTON
-//	ConfigGPButton();
-//
-//	//CONFIG LEDS
-//	//led 0
-//	ConfigLED0();
-//	//led 1
-//	ConfigLED1();
-//	//CONFIG 12V SWITCH
-//	Config12V();
-//	conv_watch = StartStopWatch(50000);
-//	conv_timer = StartStopWatch(500);
+	I2CA_Init();
+
+	devAddr = MPU6050_DEFAULT_ADDRESS;
+
+	mpu_reset();
+	int id = getDeviceID();
+	setSleepEnabled(false);
+	char status = dmpInitialize();
+
+    setFullScaleGyroRange(MPU6050_GYRO_FS_2000);
+    setFullScaleAccelRange(MPU6050_ACCEL_FS_4);
+
+
+//    setXGyroOffset(-60);
+//    setYGyroOffset(-30);
+//    setZGyroOffset(-35);
+//    setZAccelOffset(0);
+//   setXAccelOffset(0);
+//   setYAccelOffset(0);
+
+
+
+	adcinit();
+
+	//CONFIG GP_BUTTON
+	ConfigGPButton();
+
+	//CONFIG LEDS
+	//led 0
+	ConfigLED0();
+	//led 1
+	ConfigLED1();
+	//CONFIG 12V SWITCH
+	Config12V();
+	conv_watch = StartStopWatch(50000);
+	conv_timer = StartStopWatch(50);
 }
 
 
@@ -130,41 +129,41 @@ void SensorCovMeasure()
 	//use stopwatch to catch timeouts
 	//waiting should poll isStopWatchComplete() to catch timeout and throw StopWatchError
 
-//	if(isStopWatchComplete(conv_timer) == 0)
-//	{
-//		return;
-//	}
-//
-//
-//	StopWatchRestart(conv_timer);
-//
-//	mpuIntStatus = getIntStatus();
-//	fifoCount = getFIFOCount();
-//
-//	getMotion6(&data_temp.ax, &data_temp.ay, &data_temp.az, &data_temp.gx, &data_temp.gy, &data_temp.gz);
-//	if(fifoCount >= packetSize)
-//	{
-//		if (fifoCount >= 1024)
-//		{
-//			// reset so we can continue cleanly
-//			resetFIFO();
-//		}
-//		else if (mpuIntStatus & 1)
-//		{
-//			// wait for correct available data length, should be a VERY short wait
-//			while (fifoCount < packetSize) fifoCount = getFIFOCount();
-//
-//			StopWatchRestart(conv_watch);
-//
-//			getFIFOBytes(fifoBuffer, packetSize);
-//            dmpGetQuaternion(&data_temp.q, fifoBuffer);
-//            dmpGetAccel(&data_temp.aa, fifoBuffer);
-//            dmpGetGravity(&data_temp.gravity, &data_temp.q);
-//            dmpGetYawPitchRoll(data_temp.ypr, &data_temp.q, &data_temp.gravity);
-//			//probably not necessary
-//			//fifoCount -= packetSize;
-//		}
-//	}
+	if(isStopWatchComplete(conv_timer) == 0)
+	{
+		return;
+	}
+
+
+	StopWatchRestart(conv_timer);
+
+	mpuIntStatus = getIntStatus();
+	fifoCount = getFIFOCount();
+
+	getMotion6(&data_temp.ax, &data_temp.ay, &data_temp.az, &data_temp.gx, &data_temp.gy, &data_temp.gz);
+	if(fifoCount >= packetSize)
+	{
+		if (fifoCount >= 1024)
+		{
+			// reset so we can continue cleanly
+			resetFIFO();
+		}
+		else if (mpuIntStatus & 1)
+		{
+			// wait for correct available data length, should be a VERY short wait
+			while (fifoCount < packetSize) fifoCount = getFIFOCount();
+
+			StopWatchRestart(conv_watch);
+
+			getFIFOBytes(fifoBuffer, packetSize);
+            dmpGetQuaternion(&data_temp.q, fifoBuffer);
+            dmpGetAccel(&data_temp.aa, fifoBuffer);
+            dmpGetGravity(&data_temp.gravity, &data_temp.q);
+            dmpGetYawPitchRoll(data_temp.ypr, &data_temp.q, &data_temp.gravity);
+			//probably not necessary
+			//fifoCount -= packetSize;
+		}
+	}
 
 
 }
